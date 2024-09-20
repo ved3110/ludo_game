363ecb2844a23fe95f45f17cf66bafed09bd1c46
HP <HP@172.20.25.207>
2024-09-20 15:59:21 +0530

    initial commit

---
+++ ludogame/src/ludogame/GameScreen.java
@@ -0,0 +1,20 @@
+package ludogame;
+import javax.swing.JFrame;
+public class GameScreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 JFrame jframe = new JFrame();
+	        jframe.setBounds(10,10,1000,600);
+	        jframe.setTitle("LUDO");
+	        jframe.setResizable(false);
+	        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        GameMoves gm = new GameMoves();
+	        gm.setFocusable(true);
+	        gm.addKeyListener(gm);
+	        gm.addMouseListener(gm);
+	        jframe.add(gm);
+	        jframe.setVisible(true);
+	}
+
+}
